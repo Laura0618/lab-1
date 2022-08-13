cfc5501b83da65939c291c92848e552a46a8a691
Laura Castro <castrolaura0311@gmail.com>
2022-08-13 15:01:50 +0200

    Commit

---
+++ src/components/NewComponent.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+<H1>Este es un componente</H1>
+<p> El numero actual es {{ numero }}</p>
+<button @click="sumar">Sumar</button>
+<button @click="restar">Restar</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                numero: 0,
+            }
+        },
+        methods: {
+            sumar() {
+                this.numero++
+            },
+            restar() {
+                this.numero--
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <NewComponent/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import NewComponent from '@/components/NewComponent.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    NewComponent
+  }
+}
+</script>
